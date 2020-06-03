3a037ab7948c28edb7cf23fa063649f7387540f2
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-02 21:12:30 -0700

    Correct class of animal-talk, rename to class dog-talk, delete comment, add jQuery to remove any paragraph from div before adding in new paragraph.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   $("button#cat-button").click(function() {
+    $(".cat-talk > p").remove();
+    $(".dog-talk > p").remove();
     $("button#cat-button").after("<p>Hey Dog! What's up?!</p>");
     $("button#dog-button").after("<p>Not much, Cat! How are you today?</p>");
-    alert("Hi");
   });
 
   $("button#dog-button").click(function() {
+    $(".dog-talk > p").remove();
+    $(".cat-talk > p").remove();
     $("button#dog-button").after("<p>How's it hanging my feline friend?</p>");
     $("button#cat-button").after("<p>Just leading a revolution of the Proletariat! We have nothing to lose but our chains!</p>");
-    alert("Hello");
   });
 
 });
